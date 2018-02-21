88f475d0ea4ccee5ea4cc257d5645282b16a0c5c
Sergio del Amo <sergio.delamo@softamo.com>
2018-02-21 18:38:12 +0100

    use after

--- complete/grails-app/controllers/demo/BookShowInterceptor.groovy
+++ complete/grails-app/controllers/demo/BookShowInterceptor.groovy
@@ -5,14 +5,16 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class BookShowInterceptor {
 
+
     // TODO Inject service which pbulishes messages to Rabbit
 
     BookShowInterceptor() {
         match(controller:"book", action:"show")
     }
 
-    boolean before() {
+    boolean after() {
         // TODO publish message via the injected service
+        // Book book = model.bookInstance
 
         true
     }
